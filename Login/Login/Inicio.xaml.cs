120f6e53c29c33c01580987ea08058ac93041507
rache <rache@RAQUEL>
2025-04-05 19:48:35 -0600

    Agregar archivos de proyecto.

---
+++ Login/Login/Inicio.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string password = txtPw.Text;
+            if (usuario == "admin" && password == "1234")
+            {
+                Navigation.PushAsync(new Dashboard());
+            }
+            else
+            {
+                lblError.Text = "Usuario o contraseña incorrectos";
+            }
+        }
+    }
+}
